bb7ff2294c9106e40d173142f38f1700f7e06e9a
Fasara <fabiana.asara@gmail.com>
2026-06-02 16:54:17 +0100

    fix: use import instead of requre

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -1,8 +1,12 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 async function run() {
+    try {
     core.info('Deploying to AWS S3 bucket...');
+    } catch (error) {
+        core.setFailed(error instanceof Error ? error.message : String(error));
+    }
 }
 
 run();
