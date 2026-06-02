e6c6dceae4f3093e805b1c3acd696e61b3666736
Fasara <fabiana.asara@gmail.com>
2026-06-02 17:58:09 +0100

    Fix error

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import { exec } from '@actions/exec';
+import * as exec from '@actions/exec';
 
 async function run() {
     // Get inputs
@@ -10,8 +10,6 @@ async function run() {
     const s3Uri = `s3://${bucket}`;
 
     // Upload files
-    process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-    process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
     await exec.exec(`aws s3 sync ${distPath} ${s3Uri} --region ${region}`);
 
 }
